bc84bb89c69c71ed0e5a1fcc4a83d9ccb9b55563
farwish <farwish@foxmail.com>
2020-11-24 01:08:28 +0800

    update package its suites

--- app/Nova/Package.php
+++ app/Nova/Package.php
@@ -3,11 +3,13 @@
 namespace App\Nova;
 
 use App\Models\Package as PackageModel;
+use Benjacho\BelongsToManyField\BelongsToManyField;
 use Hubertnnn\LaravelNova\Fields\DynamicSelect\DynamicSelect;
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use OptimistDigital\MultiselectField\Multiselect;
@@ -54,10 +56,26 @@ class Package extends Resource
         return [
             ID::make(__('ID'), 'id')->sortable(),
 
-            DynamicSelect::make('科目分类', 'category_id')
-                ->options($this->categoryTree())
-                ->rules('required')
+            // DynamicSelect::make('科目分类', 'category_id')
+            //     ->options($this->categoryTree())
+            //     ->rules('required')
+            //     ->onlyOnForms()
+            // ,
+            // Select::make('Category', 'category')
+            //     ->options($this->categoryTree())
+            //     ->rules('required')
+            //     ->searchable()
+            //     ->onlyOnForms()
+            // ,
+
+            // Only relation can be used by BelongsToManyField::dependOn
+            BelongsTo::make('科目分类', 'category', Category::class)
                 ->onlyOnForms()
+                ->searchable()
+                ->displayUsing(function ($model) {
+                    $tree = $this->categoryTree();
+                    return $model ? $tree[$model->getAttribute('id')] : $tree;
+                })
             ,
 
             BelongsTo::make('科目分类', 'category', Category::class)
@@ -86,13 +104,29 @@ class Package extends Resource
                 })
             ,
 
+            // Suite:
+
+            // Only for create/edit, for help with 'Multiselect'.
+            BelongsToManyField::make('试卷组', 'suites', Suite::class)
+                ->dependsOn('category', 'category_id')
+                ->onlyOnForms()
+            ,
+            // Only for show, because 'Multiselect' can not dependOn category relationShip.
             Multiselect::make('试卷组', 'suites')
                 ->rules('required')
                 ->belongsToMany(Suite::class)
+                ->exceptOnForms()
             ,
 
+            // Video:
+
+            BelongsToManyField::make('视频', 'videos', Video::class)
+                ->dependsOn('category', 'category_id')
+                ->onlyOnForms()
+            ,
             Multiselect::make('视频', 'videos')
                 ->belongsToMany(Video::class)
+                ->exceptOnForms()
             ,
         ];
     }
